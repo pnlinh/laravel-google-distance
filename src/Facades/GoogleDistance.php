513338b553bcafa71a4a8294ccd95239d047bf7b
Pham Ngoc Linh <pnlinh1207@gmail.com>
2018-11-01 15:13:08 +0700

    Update facades

--- src/Facades/GoogleDistance.php
+++ src/Facades/GoogleDistance.php
@@ -3,12 +3,11 @@
 namespace Pnlinh\GoogleDistance\Facades;
 
 use Illuminate\Support\Facades\Facade;
-use Pnlinh\GoogleDistance\DistanceApi;
 
 class GoogleDistance extends Facade
 {
     protected static function getFacadeAccessor()
     {
-        return DistanceApi::class;
+        return 'distance';
     }
 }
