48c5c7adf71d0e19576847c609e81f8c11cb440a
pnlinh <pnlinh1207@gmail.com>
2018-11-01 15:47:14 +0000

    Apply fixes from StyleCI

--- src/helpers.php
+++ src/helpers.php
@@ -2,12 +2,13 @@
 
 use Pnlinh\GoogleDistance\Facades\GoogleDistance;
 
-if (! function_exists('google_distance')) {
+if (!function_exists('google_distance')) {
     /**
      * @author pnlinh
      *
      * @param $origins
      * @param $destinations
+     *
      * @return string
      */
     function google_distance($origins, $destinations)
