8ab6351313003c1e94eed6a6e75081d6dfbede5e
Ngoc Linh Pham <pnlinh1207@gmail.com>
2023-01-10 23:41:27 +0700

    feat: add example units test

---
+++ tests/Unit/ExampleTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Pnlinh\GoogleDistance\Tests\Unit;
+
+use Pnlinh\GoogleDistance\Tests\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function test_that_true_is_true()
+    {
+        $this->assertTrue(true);
+    }
+}
